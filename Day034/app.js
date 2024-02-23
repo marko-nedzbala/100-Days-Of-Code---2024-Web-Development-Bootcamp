b4abe52d1328c3bd321e3abae41e5b56f0d60fb0
Marko Nedzbala <marko.nedzbala@gmail.com>
2024-02-23 13:01:04 -0500

    Day 034 Complete

--- Day034/app.js
+++ Day034/app.js
@@ -1,1 +1,32 @@
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = 'https://google.com';
+
+let h1 = document.querySelector('h1');
+console.dir(h1);
+
+// create an element
+let newElement = document.createElement('a');
+
+// set the element contentsß
+newElement.href = "https://google.com";
+newElement.textContent = "This leads elsewhere";
+
+// get access to the parent element that should hold the new element
+let firstParagraph = document.querySelector('p');
+
+// insert the new element into the parent element content
+firstParagraph.append(newElement);
+
+let paragraphElement = document.querySelector('p');
+function changeParagraphText() {
+    if (paragraphElement.textContent == 'Clicked') {
+        paragraphElement.textContent = 'UnClicked';
+    } else {
+        paragraphElement.textContent = 'Clicked';
+    }
+}
+paragraphElement.addEventListener('click', changeParagraphText);
+
+
+
+
+
