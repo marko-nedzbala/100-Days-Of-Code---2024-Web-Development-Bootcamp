cc29376b2a0e9f106b830bbc3e4e18dbe24a7259
Marko Nedzbala <marko.nedzbala@gmail.com>
2024-02-24 18:07:31 -0500

    Day 038 Complete

--- Day038/app.js
+++ Day038/app.js
@@ -1,22 +1,30 @@
-let productNameInputElement = document.getElementById('product-name');
-let remainingChars = document.getElementById('remaining-chars');
+const productNameInputElement = document.getElementById('product-name');
+const remainingCharsElement = document.getElementById('remaining-chars');
 
-// get the value of the max length from the Input tag
-let maxAllowedChars = productNameInputElement.maxLength;
+// console.dir(productNameInputElement);
+
+const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters(event) {
-    let enteredText = event.target.value;
-    let enteredTextLength = enteredText.length;
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length; 
 
-    let remainingCharacters = maxAllowedChars - enteredTextLength;
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
 
-    remainingChars.textContent = remainingCharacters;
+  remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters === 0) {
+    remainingCharsElement.classList.add('error');
+    productNameInputElement.classList.add('error');
+  } else if (remainingCharacters <= 10) {
+    remainingCharsElement.classList.add('warning');
+    productNameInputElement.classList.add('warning');
+    remainingCharsElement.classList.remove('error');
+    productNameInputElement.classList.remove('error');
+  } else {
+    remainingCharsElement.classList.remove('warning');
+    productNameInputElement.classList.remove('warning');
+  }
 }
 
 productNameInputElement.addEventListener('input', updateRemainingCharacters);
-
-
-
-
-
-
