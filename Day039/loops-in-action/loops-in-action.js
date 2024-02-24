73ad721a99da43ba7bcc276c0ebd3a0433693bc4
Marko Nedzbala <marko.nedzbala@gmail.com>
2024-02-24 18:48:51 -0500

    Day 039 Complete

---
+++ Day039/loops-in-action/loops-in-action.js
@@ -0,0 +1,103 @@
+
+const calculateSumButtonElement = document.querySelector('#calculator button');
+
+function calculateSum() {
+    const userNumberInputElement = document.getElementById('user-number');
+    const enteredNumber = userNumberInputElement.value;
+
+    let sumUpToNumber = 0;
+
+    for (let i = 0; i <= enteredNumber; i++) {
+        sumUpToNumber += i;
+    }
+
+    const outputResultElement = document.getElementById('calculated-sum');
+
+    outputResultElement.textContent = sumUpToNumber;
+    outputResultElement.style.display = 'block';
+}
+
+calculateSumButtonElement.addEventListener('click', calculateSum);
+
+// Highlight links
+const highlightLinksButtonElement = document.querySelector('#highlight-links button');
+
+function highlightLinks() {
+    const anchorElements = document.querySelectorAll('#highlight-links a');
+
+    for (const anchorElement of anchorElements) {
+        anchorElement.classList.add('highlight');
+    }
+}
+
+highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+// display data
+const dummyUserData = {
+    firstName: 'Bob',
+    lastName: 'Smith',
+    age: 300
+};
+
+const displayUserDataButtonElement = document.querySelector('#user-data button');
+
+function displayUserData() {
+    const outputDataElement = document.getElementById('output-user-data');
+
+    outputDataElement.innerHTML = '';
+
+    for (const key in dummyUserData) {
+        const newUserDataListItem = document.createElement('li');
+        
+        const outputText = key.toUpperCase() + ':' + dummyUserData[key];
+        newUserDataListItem.textContent = outputText;
+        outputDataElement.append(newUserDataListItem);
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData);
+
+// Statistics
+
+const rollDiceButtonElement = document.querySelector('#statistics button');
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+
+}
+
+function deriveNumberOfDiceRolls() {
+    const targetNumberInputElement = document.getElementById('user-target-number');
+    const diceRollsListElement = document.getElementById('dice-rolls');
+    const enteredNumber = +targetNumberInputElement.value;
+    diceRollsListElement.innerHTML = '';
+
+    let hasRolledTargetNumber = false;
+    let numberOfRolls = 0;
+
+    while (!hasRolledTargetNumber) {
+        const rolledNumber = rollDice();
+        // if (rolledNumber === enteredNumber) {
+        //     hasRolledTargetNumber = true;
+        // }
+        numberOfRolls++;
+        const newRollListElement = document.createElement('li');
+        const outputText = 'Roll ' + numberOfRolls + ': ' + rolledNumber;
+        newRollListElement.textContent = outputText;
+        diceRollsListElement.append(newRollListElement);
+        hasRolledTargetNumber = rolledNumber == enteredNumber;
+    }
+    const outputTotalRollsElement = document.getElementById('output-total-rolls');
+    const outputTargetNumberElement = document.getElementById('output-target-number');
+
+    outputTargetNumberElement.textContent = enteredNumber;
+    outputTotalRollsElement.textContent = numberOfRolls;
+}
+
+rollDiceButtonElement.addEventListener('click', deriveNumberOfDiceRolls);
+
+
+
+
+
+
