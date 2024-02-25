3f6fd5e7802abca1e4cf1d4d7c885186abbf2828
Marko Nedzbala <marko.nedzbala@gmail.com>
2024-02-24 22:30:02 -0500

    Day 042 Start

---
+++ Day042/scripts/config.js
@@ -0,0 +1,44 @@
+
+function openPlayerConfig() {
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
